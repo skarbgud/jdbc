718de95c882eac9192dbe9a02e7727e4907ae8bf
GyuHyeong Nam <sjarbgud@gmail.com>
2022-11-04 23:45:56 +0900

    트랜잭션 - 적용2

---
+++ src/test/java/hello/jdbc/service/MemberServiceV2Test.java
@@ -0,0 +1,85 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositroyV1;
+import hello.jdbc.repository.MemberRepositroyV2;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+/**
+ * 트랜잭션 - 커넥션 파라미터 전달 방식 동기화
+ */
+class MemberServiceV2Test {
+
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberB";
+    public static final String MEMBER_EX = "ex";
+
+    private MemberRepositroyV2 memberRepositroy;
+    private MemberServiceV2 memberService;
+
+    @BeforeEach
+    void before() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        memberRepositroy = new MemberRepositroyV2(dataSource);
+        memberService = new MemberServiceV2(dataSource, memberRepositroy);
+    }
+
+    @AfterEach
+    void after() throws SQLException {
+        memberRepositroy.delete(MEMBER_A);
+        memberRepositroy.delete(MEMBER_B);
+        memberRepositroy.delete(MEMBER_EX);
+    }
+
+    @Test
+    @DisplayName("정상 이체")
+    void accountTransfer() throws SQLException {
+        //given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberB = new Member(MEMBER_B, 10000);
+        memberRepositroy.save(memberA);
+        memberRepositroy.save(memberB);
+
+        //when
+        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+
+        //then
+        Member findMemberA = memberRepositroy.findById(memberA.getMemberId());
+        Member findMemberB = memberRepositroy.findById(memberB.getMemberId());
+
+        Assertions.assertThat(findMemberA.getMoney()).isEqualTo(8000);
+        Assertions.assertThat(findMemberB.getMoney()).isEqualTo(12000);
+    }
+
+    @Test
+    @DisplayName("이체중 예외 발생")
+    void accountTransferEx() throws SQLException {
+        //given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberEx = new Member(MEMBER_EX, 10000);
+        memberRepositroy.save(memberA);
+        memberRepositroy.save(memberEx);
+
+        //when
+        assertThatThrownBy(() -> memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(), 2000))
+                .isInstanceOf(IllegalStateException.class);
+
+        //then
+        Member findMemberA = memberRepositroy.findById(memberA.getMemberId());
+        Member findMemberB = memberRepositroy.findById(memberEx.getMemberId());
+
+        // 롤백이 되기 때문에 수행전인 10000원,10000원으로 된다
+        Assertions.assertThat(findMemberA.getMoney()).isEqualTo(10000);
+        Assertions.assertThat(findMemberB.getMoney()).isEqualTo(10000);
+    }
+}
